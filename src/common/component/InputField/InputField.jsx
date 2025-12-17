8c16fb68dc030b6073e89b1eb4d045b7272655ee
Anjana Induranga <anjanananayakkara1212@gmail.com>
2025-12-17 11:24:40 +0530

    add some features

--- src/common/component/InputField/InputField.jsx
+++ src/common/component/InputField/InputField.jsx
@@ -1,21 +1,54 @@
-import React from 'react'
-import { TextField, InputAdornment } from "@mui/material";
+import React from "react";
+import { TextField, InputAdornment, Typography, Box } from "@mui/material";
 
 export default function InputField({ label, icon, value, onChange }) {
+  const hasValue = Boolean(value); // true when user types
   return (
-    <TextField
-      label={label}
-      variant="outlined"
-      value={value}
-      onChange={onChange}
-      fullWidth
-      InputProps={{
-        startAdornment: (
-          <InputAdornment position="start">
-            {icon}
-          </InputAdornment>
-        ),
-      }}
-    />
-  )
+    <Box>
+      {/* External Label */}
+      <Typography
+        variant="body2"
+        sx={{
+          mb: 0.5,
+          fontWeight: 500,
+          color: hasValue ? "#000" : "#707070",
+        }}
+      >
+        {label}
+      </Typography>
+
+      {/* Input Field */}
+      <TextField
+        variant="outlined"
+        value={value}
+        onChange={onChange}
+        fullWidth
+        InputProps={{
+          startAdornment: icon && (
+            <InputAdornment
+              position="start"
+              sx={{ color: hasValue ? "#000" : "#707070" }}
+            >
+              {icon}
+            </InputAdornment>
+          ),
+        }}
+        sx={{
+          "& .MuiOutlinedInput-root": {
+            color: hasValue ? "#000" : "#707070",
+
+            "& fieldset": {
+              borderColor: hasValue ? "#000" : "#707070",
+            },
+            "&:hover fieldset": {
+              borderColor: hasValue ? "#000" : "#707070",
+            },
+            "&.Mui-focused fieldset": {
+              borderColor: "#000",
+            },
+          },
+        }}
+      />
+    </Box>
+  );
 }
--- src/pages/FormPage1/FormPage1.jsx
+++ src/pages/FormPage1/FormPage1.jsx
@@ -4,10 +4,18 @@ import ProgressBar from "../../common/component/ProgressShortBar/ProgressBar";
 import "./style.css";
 import Footer from "../../common/component/Footer/Footer";
 import { Box } from "@mui/material";
-import InputLabel from "@mui/material/InputLabel";
+import TextField from "@mui/material/TextField";
 import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
 import Select from "@mui/material/Select";
+import PersonIcon from "@mui/icons-material/Person";
+import InputField from "../../common/component/InputField/InputField";
+import { useState } from "react";
+import dayjs from "dayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+
 
 export default function FormPage1() {
   const [age, setAge] = React.useState("");
@@ -15,6 +23,11 @@ export default function FormPage1() {
   const handleChange = (event) => {
     setAge(event.target.value);
   };
+
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [dob, setDob] = React.useState(null);
+
   return (
     <div className="content">
       <NavBar progressbar={<ProgressBar label={"My details"} />} />
@@ -30,24 +43,76 @@ export default function FormPage1() {
                 autoWidth
                 inputProps={{ "aria-label": "Without label" }}
                 sx={{
+                  // text color
+                  color: age ? "#000" : "#707070",
+
+                  // outline
+                  "& .MuiOutlinedInput-notchedOutline": {
+                    borderColor: age ? "#000" : "#707070",
+                  },
+                  "&:hover .MuiOutlinedInput-notchedOutline": {
+                    borderColor: age ? "#000" : "#707070",
+                  },
+                  "&.Mui-focused .MuiOutlinedInput-notchedOutline": {
+                    borderColor: "#000",
+                  },
+
+                  // dropdown icon
                   "& .MuiSelect-icon": {
+                    color: age ? "#000" : "#707070",
                     right: "auto",
                     left: 8,
                   },
+
+                  // text padding (icon on left)
                   "& .MuiSelect-select": {
                     paddingLeft: "32px",
                   },
                 }}
               >
                 <MenuItem value="">
-                  <em>None</em>
+                  <em style={{ color: "#707070" }}>None</em>
                 </MenuItem>
                 <MenuItem value={20}>Mr.</MenuItem>
                 <MenuItem value={21}>Mrs</MenuItem>
                 <MenuItem value={22}>Miss</MenuItem>
               </Select>
             </FormControl>
+
+            <InputField
+              label="First Name"
+              icon={<PersonIcon />}
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+            />
           </div>
+          <InputField
+            label="Last Name"
+            icon={<PersonIcon />}
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
+          />
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <Box sx={{ width: 300 }}>
+              <DatePicker
+                label="My date of birth"
+                value={dob}
+                onChange={(newValue) => setDob(newValue)}
+                format="DD-MM-YYYY"
+                maxDate={dayjs()} // prevent future dates
+                slotProps={{
+                  textField: {
+                    fullWidth: true,
+                    InputProps: {
+                      startAdornment: (
+                        <PersonIcon sx={{ mr: 1, color: "action.active" }} />
+                      ),
+                    },
+                  },
+                }}
+              />
+            </Box>
+          </LocalizationProvider>
         </Box>
       </Box>
       <Footer />
